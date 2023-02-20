c82f638bc9119a5179b2ee9d55f7a935f8d4d068
Ugur <ugurkarimli1@gmail.com>
2023-02-20 14:32:14 +0400

    MODEL MAPPER initialization.

--- src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
+++ src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
@@ -3,39 +3,38 @@ package kodlama.io.rentACar.business.concretes;
 import kodlama.io.rentACar.business.abstracts.BrandService;
 import kodlama.io.rentACar.business.requests.CreateBrandRequest;
 import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
+import kodlama.io.rentACar.core.utilities.ModelMapperService;
 import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
 import kodlama.io.rentACar.entities.concretes.Brand;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor
 public class BrandManager implements BrandService {
 
-    private BrandRepository brandRepository;
+    private final ModelMapperService mapperService;
 
-    @Autowired
-    public BrandManager(BrandRepository brandRepository) {
-        this.brandRepository = brandRepository;
-    }
+    private BrandRepository brandRepository;
 
     @Override
     public List<GetAllBrandsResponse> getAll() {
         List<Brand> brands = brandRepository.findAll();
-        List<GetAllBrandsResponse> brandsResponse = new ArrayList<>();
 
-        for (Brand brand:brands){
-            brandsResponse.add(new GetAllBrandsResponse(brand.getId(),brand.getName()));
-        }
-
-        return brandsResponse;
+        return brands
+                .stream()
+                .map(brand -> mapperService.forResponse().map(brand, GetAllBrandsResponse.class))
+                .collect(Collectors.toList());
 
     }
 
     @Override
     public void add(CreateBrandRequest createBrandRequest) {
-        this.brandRepository.save(new Brand(null,createBrandRequest.getName()));
+        brandRepository.save(mapperService.forRequest().map(createBrandRequest,Brand.class));
     }
 }
