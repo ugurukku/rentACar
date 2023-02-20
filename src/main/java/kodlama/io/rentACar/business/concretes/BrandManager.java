be32caf1f832b0fcf19d679b464dbf7f380671d8
Ugur <ugurkarimli1@gmail.com>
2023-02-20 14:17:45 +0400

    Initial commit

---
+++ src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
@@ -0,0 +1,41 @@
+package kodlama.io.rentACar.business.concretes;
+
+import kodlama.io.rentACar.business.abstracts.BrandService;
+import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
+import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
+import kodlama.io.rentACar.entities.concretes.Brand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BrandManager implements BrandService {
+
+    private BrandRepository brandRepository;
+
+    @Autowired
+    public BrandManager(BrandRepository brandRepository) {
+        this.brandRepository = brandRepository;
+    }
+
+    @Override
+    public List<GetAllBrandsResponse> getAll() {
+        List<Brand> brands = brandRepository.findAll();
+        List<GetAllBrandsResponse> brandsResponse = new ArrayList<>();
+
+        for (Brand brand:brands){
+            brandsResponse.add(new GetAllBrandsResponse(brand.getId(),brand.getName()));
+        }
+
+        return brandsResponse;
+
+    }
+
+    @Override
+    public void add(CreateBrandRequest createBrandRequest) {
+        this.brandRepository.save(new Brand(null,createBrandRequest.getName()));
+    }
+}
