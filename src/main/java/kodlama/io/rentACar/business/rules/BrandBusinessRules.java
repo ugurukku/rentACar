4fd1fa5229d99fcf22178f155ceff67b0647f62d
Ugur <ugurkarimli1@gmail.com>
2023-03-11 00:11:05 +0400

    adding business rules

--- src/main/java/kodlama/io/rentACar/business/rules/BrandBusinessRules.java
+++ src/main/java/kodlama/io/rentACar/business/rules/BrandBusinessRules.java
@@ -1,4 +1,20 @@
 package kodlama.io.rentACar.business.rules;
 
+import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
 public class BrandBusinessRules {
+
+    private final BrandRepository brandRepository;
+
+    public void checkIfBrandNameExists(String brandName){
+        if (brandRepository.existsByName(brandName)){
+
+        }
+    }
+
+
 }
