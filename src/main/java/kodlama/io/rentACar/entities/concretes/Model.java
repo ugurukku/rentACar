4e9fe49691730e121f6db245ac38a64d245d68d1
Ugur <ugurkarimli1@gmail.com>
2023-03-10 17:28:27 +0400

    ADDED APP PROPERTIES

---
+++ src/main/java/kodlama/io/rentACar/entities/concretes/Model.java
@@ -0,0 +1,28 @@
+package kodlama.io.rentACar.entities.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Table(name = "models")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Model {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "brand_id")
+    private Brand brand;
+
+}
